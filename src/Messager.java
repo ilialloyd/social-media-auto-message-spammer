b348838a67693517af091923f1ce38941ab220fb
Ilham Mammadli <by.memmedli@gmail.com>
2023-02-26 23:08:25 -0500

    correction

--- src/Messager.java
+++ src/Messager.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * Leave your comment below if you have
  * --------------------------------------------------------
  */
-public class Main {
+public class Messager {
     public static void main(String[] args) throws AWTException, InterruptedException {
 
         //message start Count
